e097a8d05eeee9d2d057939e628fb73a989ffb72
demir <46607042+swordfishtr@users.noreply.github.com>
2024-10-09 00:06:24 +0300

    store storage keys as const + popup message tweak

--- popup/popup.js
+++ popup/popup.js
@@ -5,22 +5,30 @@ const months = new Map([[1, 'jan'], [2, 'feb'], [3, 'mar'], [4, 'apr'], [5, 'may
 const years = [2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030];
 const currentDate = new Date();
 
+const KEY_TOGGLESTATE = "35pokes-toggleState";
+const KEY_MONTH = "35pokes-month";
+const KEY_YEAR = "35pokes-year";
+const KEY_TEXT = "35pokes-text";
+const KEY_LIST = "35pokes-list";
+// Keys to be deleted from storage for 1.4:
+// "35pokes-code"
+
 document.addEventListener("DOMContentLoaded", function() {
     const messageContainer = document.getElementById('message-container');
 
     // Toggle
     const toggleElement = document.getElementById('toggle-input');
-    browser.storage.local.get(['35pokes-toggleState']).then(items => {
-        if (items['35pokes-toggleState']) {
-            toggleElement.value = items['35pokes-toggleState'];
-            toggleElement.checked = items['35pokes-toggleState'];
+    browser.storage.local.get(KEY_TOGGLESTATE).then(items => {
+        if (items[KEY_TOGGLESTATE]) {
+            toggleElement.value = items[KEY_TOGGLESTATE];
+            toggleElement.checked = items[KEY_TOGGLESTATE];
         } else {
             toggleElement.value = false;
             toggleElement.checked = false;
         }
     });
     toggleElement.addEventListener('change', function() {
-        browser.storage.local.set({ '35pokes-toggleState': this.checked });
+        browser.storage.local.set({ [KEY_TOGGLESTATE]: this.checked });
         messageTabs({ toggleSet: this.checked });
         fetchAllowedPokemonDataAndSetStorage().then(onPopupActivity); // remove this for 1.4
     });
@@ -33,16 +41,16 @@ document.addEventListener("DOMContentLoaded", function() {
         opt.innerHTML = value;
         monthElement.appendChild(opt);
     });
-    browser.storage.local.get(['35pokes-month']).then(items => {
-        if (items['35pokes-month']) {
-            monthElement.value = items['35pokes-month'];
+    browser.storage.local.get(KEY_MONTH).then(items => {
+        if (items[KEY_MONTH]) {
+            monthElement.value = items[KEY_MONTH];
         } else {
             monthElement.value = currentDate.getUTCMonth() + 1;
-            browser.storage.local.set({ '35pokes-month': currentDate.getUTCMonth() + 1 });
+            browser.storage.local.set({ [KEY_MONTH]: currentDate.getUTCMonth() + 1 });
         }
     });
     monthElement.addEventListener('change', function() {
-        browser.storage.local.set({ '35pokes-month': Number(this.value) });
+        browser.storage.local.set({ [KEY_MONTH]: Number(this.value) });
         fetchAllowedPokemonDataAndSetStorage().then(onPopupActivity);
     });
 
@@ -54,45 +62,45 @@ document.addEventListener("DOMContentLoaded", function() {
         opt.innerHTML = y;
         yearElement.appendChild(opt);
     });
-    browser.storage.local.get(['35pokes-year']).then(items => {
-        if (items['35pokes-year']) {
-            yearElement.value = items['35pokes-year'];
+    browser.storage.local.get(KEY_YEAR).then(items => {
+        if (items[KEY_YEAR]) {
+            yearElement.value = items[KEY_YEAR];
         } else {
             yearElement.value = currentDate.getUTCFullYear();
-            browser.storage.local.set({ '35pokes-year': currentDate.getUTCFullYear() });
+            browser.storage.local.set({ [KEY_YEAR]: currentDate.getUTCFullYear() });
         }
     });
     yearElement.addEventListener('change', function() {
-        browser.storage.local.set({ '35pokes-year': Number(this.value) });
+        browser.storage.local.set({ [KEY_YEAR]: Number(this.value) });
         fetchAllowedPokemonDataAndSetStorage().then(onPopupActivity);
     });
 
     // Text Input
     const textElement = document.getElementById('text-input');
-    browser.storage.local.get(['35pokes-text']).then(items => {
-        if (items['35pokes-text']) {
-            textElement.value = items['35pokes-text'];
+    browser.storage.local.get(KEY_TEXT).then(items => {
+        if (items[KEY_TEXT]) {
+            textElement.value = items[KEY_TEXT];
         } else {
             textElement.value = '';
-            browser.storage.local.set({ '35pokes-text': '' });
+            browser.storage.local.set({ [KEY_TEXT]: '' });
         }
     });
     textElement.addEventListener('change', function() {
-        browser.storage.local.set({ '35pokes-text': this.value });
+        browser.storage.local.set({ [KEY_TEXT]: this.value });
         fetchAllowedPokemonDataAndSetStorage().then(onPopupActivity);
     });
 
     const buttonElement = document.getElementById('code-button');
     buttonElement.addEventListener('click', function() {
         const challengeCodePrefix = '/challenge gen9nationaldexag @@@ Z-Move Clause, -Mega, Terastal Clause, Sleep Clause Mod, Forme Clause, -Hidden Power, -Last Respects, -Kings Rock, -Shadow Tag, -Acupressure, -Battle Bond, -Quick Claw, -Razor Fang, Evasion Clause, OHKO Clause, baton pass stat trap clause, -All Pokemon, +';
-        browser.storage.local.get('35pokes-list').then(items => {
-            if(items['35pokes-list']) navigator.clipboard.writeText(challengeCodePrefix + items['35pokes-list'].join(', +')).then(() => showMessage('Copied!'));
+        browser.storage.local.get(KEY_LIST).then(items => {
+            if(items[KEY_LIST]) navigator.clipboard.writeText(challengeCodePrefix + items[KEY_LIST].join(', +')).then(() => showMessage('Copied!'));
             else showMessage("This meta doesn't exist yet!");
         });
     });
 
     function onPopupActivity(result) {
-        const textPrefix = result.list ? "Date set:<br>" : "Meta not found:<br>";
+        const textPrefix = result.list ? "Date set: " : "Meta not found: ";
         const textSuffix = result.text ? " " + result.text : "";
         showMessage(textPrefix + months.get(result.month) + " " + result.year + textSuffix);
         messageTabs(result);
@@ -123,22 +131,22 @@ document.addEventListener("DOMContentLoaded", function() {
 async function fetchAllowedPokemonDataAndSetStorage() {
     // Default metagame in case the storage gets wiped.
     const storageItems = await browser.storage.local.get({
-        '35pokes-month': 5,
-        '35pokes-year': 2024,
-        '35pokes-text': ''
+        [KEY_MONTH]: 5,
+        [KEY_YEAR]: 2024,
+        [KEY_TEXT]: ''
     });
     const textPrefix = 'https://samuel-peter-chowdhury.github.io/35PokesShowdownFilter/dates/';
-    const textSuffix = storageItems['35pokes-text'] ? '_' + storageItems['35pokes-text'] : '';
-    const fileName = textPrefix + storageItems['35pokes-year'] + '_' + storageItems['35pokes-month'] + textSuffix + '.json';
+    const textSuffix = storageItems[KEY_TEXT] ? '_' + storageItems[KEY_TEXT] : '';
+    const fileName = textPrefix + storageItems[KEY_YEAR] + '_' + storageItems[KEY_MONTH] + textSuffix + '.json';
 
     let allowedMons = "";
     const response = await fetch(fileName);
     if(response.ok) allowedMons = await response.json();
-    browser.storage.local.set({ '35pokes-list': allowedMons });
+    browser.storage.local.set({ [KEY_LIST]: allowedMons });
     return {
-        month: storageItems['35pokes-month'],
-        year: storageItems['35pokes-year'],
-        text: storageItems['35pokes-text'],
+        month: storageItems[KEY_MONTH],
+        year: storageItems[KEY_YEAR],
+        text: storageItems[KEY_LIST],
         list: allowedMons
     };
 }
