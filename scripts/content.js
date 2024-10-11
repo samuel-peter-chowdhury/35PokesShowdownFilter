de2797339ee07c7c1458474544483048b946bfd7
demir <demir.ab@protonmail.com>
2024-10-11 18:10:23 +0300

    lots of updates (#15)

--- scripts/content.js
+++ scripts/content.js
@@ -1,125 +1,130 @@
-let allowedMap = new Map();
-const challengeCodePrefix = '/challenge gen9nationaldexag @@@ Z-Move Clause, -Mega, Terastal Clause, Sleep Clause Mod, Forme Clause, -Hidden Power, -Last Respects, -Kings Rock, -Shadow Tag, -Acupressure, -Battle Bond, -Quick Claw, -Razor Fang, Evasion Clause, OHKO Clause, baton pass stat trap clause, -All Pokemon, +';
-
 // Polyfill for browser compatibility
-if (typeof browser === 'undefined') {
-    var browser = chrome;
-}
+if (typeof browser === "undefined") globalThis.browser = chrome;
 
-function getChallengeCode() {
-    const pokemonList = Array.from(allowedMap.keys()).join(', +');
-    return challengeCodePrefix + pokemonList;
-}
-
-// Function to fetch JSON data
-function fetchAllowedPokemonData() {
-    browser.storage.local.get('35pokes-month').then(monthItems => {
-        browser.storage.local.get('35pokes-year').then(yearItems => {
-            browser.storage.local.get('35pokes-text').then(textItems => {
-                const xhr = new XMLHttpRequest();
-                let fileName;
-                if (monthItems['35pokes-month'] && yearItems['35pokes-year']) {
-                    textSuffix = textItems['35pokes-text'] ? '_' + textItems['35pokes-text'] : '';
-                    fileName = 'https://samuel-peter-chowdhury.github.io/35PokesShowdownFilter/dates/' + yearItems['35pokes-year'] + '_' + monthItems['35pokes-month'] + textSuffix + '.json';
-                } else {
-                    fileName = 'https://samuel-peter-chowdhury.github.io/35PokesShowdownFilter/dates/2024_5.json';
-                }
-                console.log(fileName);
-                xhr.open('GET', fileName, true);
-                xhr.onreadystatechange = function() {
-                    if (xhr.readyState == 4) {
-                        if (xhr.status === 200) {
-                            const data = JSON.parse(xhr.responseText);
-                            data.forEach(item => {
-                                allowedMap.set(item.toLowerCase(), true);
-                            });
-                            browser.storage.local.set({ '35pokes-code': getChallengeCode() }, function() {});
-                            console.log(allowedMap);
-                        } else {
-                            alert('Invalid Date');
-                        }
-                    }
-                }
-                xhr.send();
-            });
-        });
-    });
-}
-
-// Call the function to fetch JSON data
-fetchAllowedPokemonData();
-
-let removedElements = [];
-
+let metaText = '';
+const allowedMap = new Map();
+const removedElements = [];
 const observer = new MutationObserver(onMutation);
-browser.storage.local.get(['35pokes-toggleState']).then(items => {
-    if (items['35pokes-toggleState']) {
-        observer.observe(document, {
+
+const KEY_TOGGLESTATE = "35pokes-toggleState";
+const KEY_TEXT = "35pokes-text";
+const KEY_LIST = "35pokes-list";
+const KEY_MONTH = "35pokes-month";
+
+// Receive settings as the user changes them.
+browser.storage.local.onChanged.addListener(items => {
+    if(items[KEY_LIST]) {
+        allowedMap.clear();
+        if(items[KEY_LIST].newValue) items[KEY_LIST].newValue.forEach(item => allowedMap.set(item.toLowerCase()));
+    }
+    else if(items[KEY_TOGGLESTATE]) {
+        if(items[KEY_TOGGLESTATE].newValue) observer.observe(document, {
             childList: true,
             subtree: true,
         });
+        else observer.disconnect();
+    }
+    else if(items[KEY_TEXT]) {
+        metaText = items[KEY_TEXT].newValue;
     }
 });
 
+// Receive settings on load from last time if present, otherwise load defaults.
+browser.storage.local.get({
+    [KEY_TOGGLESTATE]: false,
+    [KEY_TEXT]: '',
+    [KEY_LIST]: '',
+    [KEY_MONTH]: ''
+}).then(data => {
+    if(data[KEY_TEXT]) metaText = data[KEY_TEXT];
+    if(data[KEY_LIST]) data[KEY_LIST].forEach(item => allowedMap.set(item.toLowerCase()));
+    else if(data[KEY_MONTH]) {
+        // make the user open popup which populates KEY_LIST
+        browser.storage.local.set({
+            [KEY_MONTH]: '',
+            [KEY_TOGGLESTATE]: false
+        });
+        // might as well do this now, save a few lines in service worker later
+        browser.storage.local.remove("35pokes-code");
+        return;
+    }
+    if(data[KEY_TOGGLESTATE]) observer.observe(document, {
+        childList: true,
+        subtree: true,
+    });
+});
+
 function onMutation(mutations) {
+    if(allowedMap.size === 0) return;
+
     for (const { addedNodes } of mutations) {
         for (const node of addedNodes) {
-            if (!node.tagName) continue;
-            const elements = node.getElementsByClassName('teambuilder-results');
-            if (elements[0]) {
-                filterChart(elements[0]);
+            if(!node.tagName) continue;
+            const elements = node.getElementsByClassName('teambuilder-results')[0];
+            if(elements) {
+                filterChart(elements);
                 return;
             }
-            const results = node.getElementsByClassName('result');
-            if (results) {
-                for (const result of results) {
-                    const resultChild = result.querySelector('a[data-entry^="pokemon|"]');
-                    if (resultChild) {
-                        filterChart(results[0].parentElement.parentElement);
-                        return;
-                    }
-                }
+            const results = node.getElementsByClassName('result')[0];
+            if(results) {
+                filterChart(results.parentElement.parentElement);
+                return;
             }
         }
     }
 }
 
 function filterChart(chart) {
-    if (chart) {
-        console.log('Filtering Chart...');
+    if (!chart) return; 
 
-        // Mobile support
+    chart.scrollTop = chart.scrollHeight;
+    
+    // Mobile support - previous statement does nothing on mobile view.
+    if(chart.scrollTop === 0) {
         let morebtn = document.getElementsByClassName('result more');
-        if(!morebtn.length) chart.scrollTop = chart.scrollHeight;
         while(morebtn.length) morebtn[0].firstChild.firstChild.click();
-
-        setTimeout(function() {
-            chart.scrollTop = 0;
-            removedElements = [];
-            filterPokemon(chart);
-            filterHeaders(chart);
-            unsetHeight(chart);
-        }, 50);
     }
+
+    setTimeout(function() {
+        chart.scrollTop = 0;
+        removedElements.length = 0;
+
+        let chartType = filterHeaders(chart);
+        filterEntries(chart, chartType);
+        unsetHeight(chart);
+    }, 50);
 }
 
-function filterPokemon(parentElement) {
+function filterHeaders(parentElement) {
+    const results = [...parentElement.querySelectorAll('li.result')];
+    const headers = results.map((element) => element.querySelector('h3')).filter(e => e);
+    for(let value of headers) {
+        // "EVs" and "Details" don't quite work for this.
+        if(value.innerHTML === "Popular items") return "Items";
+        if(value.innerHTML === "Abilities") return "Abilities";
+        if(value.innerHTML === "Moves") return "Moves";
+        value.parentElement.style.display = 'none';
+        removedElements.push(value.parentElement);
+    }
+    return "Pokemon";
+}
+
+function filterEntries(parentElement, chartType) {
     const entries = parentElement.querySelectorAll('li.result');
-    entries.forEach(entry => {
+    if(chartType === "Pokemon") entries.forEach(entry => {
         const pokemonName = entry.querySelector('a[data-entry^="pokemon|"]')?.getAttribute('data-entry')?.split('|')[1];
         if (pokemonName && !allowedMap.has(pokemonName.toLowerCase())) {
             entry.style.display = 'none';
             removedElements.push(entry);
         }
     });
-}
-
-function filterHeaders(parentElement) {
-    const results = [...parentElement.querySelectorAll('li.result')];
-    const headers = results.map((element) => element.querySelector('h3')).filter(e => e);
-    headers.forEach(value => {
-        value.parentElement.style.display = 'none';
-        removedElements.push(value.parentElement);
+    // Don't filter out hidden power for old gens.
+    else if(chartType === "Moves") if(!/^(RBY|GSC|ADV|BW|ORAS|SM)$/.test(metaText)) entries.forEach(entry => {
+        const moveName = entry.querySelector('a[data-entry^="move|"]')?.getAttribute('data-entry')?.split('|')[1];
+        if(moveName && /Hidden Power/.test(moveName)) {
+            entry.style.display = 'none';
+            removedElements.push(entry);
+        }
     });
 }
 
